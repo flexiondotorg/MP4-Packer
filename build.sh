b6aec6c8c2796fa3610416c0e14826221101e14e
Martin Wimpress <code@flexion.org>
2009-09-20 19:01:48 +0100

    Updated release script

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 function build {
     RELEASE_NAME="MP4-Repacker"
-    RELEASE_VER=1.0
+    RELEASE_VER="1.0"
+    RELEASE_DESC="Extracts audio and video from MPEG-4 container and repacks it."
+    RELEASE_KEYWORDS="MP4, MPEG-4, Quicktime, container, repack, fix, repair"
 
     rm ${RELEASE_NAME}-v${RELEASE_VER}.tar* 2>/dev/null
     bzr export ${RELEASE_NAME}-v${RELEASE_VER}.tar
